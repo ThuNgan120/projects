016dfe91022ce07bc1b63cab9f246389be2938eb
Thu Ngân <nguyenthithungan8520@gmail.com>
2025-01-11 00:51:50 +0700

    thay dooii

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
       @user = User.new
     end
 
+    
     def create
       #   @user = User.new(user_params)
       #   if @user.save
@@ -15,15 +16,17 @@ class UsersController < ApplicationController
       #     render :new, alert: "Error creating account."
       #   end
 
+      
       @user = User.new(user_params)
-      if 
+      if @user.save
         flash[:notice] = "Account created successfully."
         redirect_to users_path
-      else @user.save
-         flash[:alert] = "Failed to create account. Please check your input."
+      else
+        flash[:alert] = "Failed to create account. Please check your input."
         render :new
       end
     end
+    
 
     private
 def user_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_10_153633) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_10_171519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -58,7 +58,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_10_153633) do
     t.datetime "updated_at", null: false
     t.integer "inventory_count"
     t.string "image_url"
-    t.decimal "price"
+    t.integer "price"
   end
 
   create_table "sessions", force: :cascade do |t|
