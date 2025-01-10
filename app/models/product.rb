4c5eea46ff04ac5279d5eebc16e3038a915ac7ad
Thu Ngân <nguyenthithungan8520@gmail.com>
2025-01-11 00:09:17 +0700

    add products

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,7 @@ class Product < ApplicationRecord
 
     validates :name, presence: true
     validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
+    
 
     after_update_commit :notify_subscribers, if: :back_in_stock?
 
